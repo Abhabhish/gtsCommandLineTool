0bed20319319f1de2f9d4f85489d72ad12640059
Ashwin Himanshu <ashwinhimanshu11@gmail.com>
2024-02-20 13:37:57 +0530

    modified renameFiles.py

--- tools/renameFiles.py
+++ tools/renameFiles.py
@@ -5,21 +5,41 @@ import csv
 def rename():
     files_to_be_renamed = {}
 
-    input_option = input("How would you like to input the file name?\n(a) Manually enter the file names\n(b) Import from CSV file\n\n>>>")
+    input_option = input("How would you like to input the file names?\n(a) Manually enter the file names one by one\n(b) Paste copied pairs of old and new file names.\n(c) Import pairs from a CSV file\n\n>>>")
 
     if input_option == 'a':
-        print("Enter the file names to be renamed: ")
-
+        
+        print("Enter the file names: ")
+        
         while True:
             current_name = input("Enter the current file name:\n>>")
             if not current_name:
                 break
             else:
-                new_name = input(f"Enter the new file name for {current_name}:\n>>")
+                new_name = input(f"Enter the new file name:\n>>")
                 files_to_be_renamed[current_name] = new_name.strip()
-
+    
     elif input_option == 'b':
+        
+        print("Paste pairs. Enter on new line to finish")
+
+        while True:
+            user_input = input(">> ").strip()
+
+            if not user_input:
+                break
+            
+            names = user_input.split()
+            if len(names) == 2:
+                old_name, new_name = names
+                files_to_be_renamed[old_name] = new_name
+            else:
+                print("Invalid input format, paste in pairs")
+
+    elif input_option == 'c':
+
         csv_file_path = input("Enter the path to the CSV file containing the new file names:\n>>")
+
         try:
             with open(csv_file_path, newline='', encoding='utf-8') as csv_file:
                 reader = csv.reader(csv_file)
@@ -28,11 +48,13 @@ def rename():
         except FileNotFoundError:
             print(f"Error: CSV file not found at {csv_file_path}")
             return
+
     else:
-        print("Invalid option. Enter 'a' or 'b'.")
+        print("Invalid option. Enter 'a', 'b' or 'c.")
         return
 
-    source = input("Enter Source:\n>>")
+
+    source = input("Enter source:\n>>")
 
     for root, dirs, files in os.walk(source):
         for file in files:
@@ -43,7 +65,7 @@ def rename():
                 new_file_path = os.path.join(root, new_file_name)
 
                 if os.path.exists(new_file_path):
-                    print(f"Error: File name already exist.")
+                    print(f"Error: File name already exists.")
                 else:
                     os.rename(old_file_path, new_file_path)
                     print(f"Renamed: {old_file_path} ----> {new_file_path}")
