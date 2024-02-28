d5113fb1a97f1948dffbd520b5723c1b98dd98a5
Ashwin Himanshu <ashwinhimanshu11@gmail.com>
2024-02-28 17:05:05 +0530

    modified pngToJpg.py and heicToJpg.py

--- tools/heicTojpg.py
+++ tools/heicTojpg.py
@@ -1,73 +1,56 @@
-from PIL import Image # pip install pillow
-from pillow_heif import register_heif_opener # pip install pillow-heif
+from PIL import Image
+from pillow_heif import register_heif_opener
 import os
+import csv
 
-register_heif_opener() # Call to work with HEIC/HEIF formats
+register_heif_opener()
 
 def heic_to_jpg():
 
-    # Setup a list to store the file names
-    files_to_be_converted = []
+    print("Choose the conversion method ->\n(a) Input source folder path\n(b) Import from a CSV file\n\n>>>")
 
-    input_option = input("How would you like to input the file names?\n(a) Manually enter file names\n(b) Paste file names\n(c) Import from CSV file\n\n>>>")
+    input_option = input(">> ")
 
-    # Enter the source
-    source = input("Enter source:\n>>")
+    source = input("Enter source of image directory:\n>>")
 
-    # Enter the destination
     destination = input("Enter destination:\n>>")
 
-    # If manual input option is chosen
     if input_option == 'a':
-        print("Enter the file names: ")
 
-        while True:
-            current_name = input("Enter the current file name:\n>>")
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                if file.lower().endswith('.heic'):
+                    convert_heic_to_jpg(os.path.join(root, file), destination)
 
-            if not current_name:
-                break
-            else:
-                convert_heic_to_jpg(os.path.join(source, current_name), destination)
-
-    # If paste multiple files name is chosen
     elif input_option == 'b':
-        print("Paste file names:")
 
-        while True:
-            user_input = input(">> ").strip()
+        csv_file_path = input("Enter the path to CSV file:\n(sample format: 1st col -> file_name)\n>> ")
 
-            if not user_input:
-                break
-
-            files_to_be_converted.append(os.path.join(source, user_input)) 
-
-            for file_name in files_to_be_converted:
-                convert_heic_to_jpg(os.path.join(source, file_name), destination)
-
-    # If import from CSV option is chosen
-    elif input_option == 'c':
-        csv_file_path = input("Enter the path to the CSV file containing the file names:\n>>")
         try:
             with open(csv_file_path, newline='', encoding='utf-8') as csv_file:
+                reader = csv.reader(csv_file)
 
-                for row in csv_file:
-                    convert_heic_to_jpg(os.path.join(source, row.strip()), destination)
-        
+                for row in reader:
+                    if row:
+                        file_name = row[0].strip()
+                        convert_heic_to_jpg(os.path.join(source, file_name), destination)
+
         except FileNotFoundError:
-            print(f"Error: CSV file not found at {csv_file_path}")
+            print(f"Error: CSV file not fount at {csv_file_path}")
             return
 
     else:
-        print("Invalid option. Enter 'a', 'b', or 'c'.")
+        print("Invalid option. Enter 'a' or 'b'.")
         return
 
 
 def convert_heic_to_jpg(source, destination):
     try:
         with Image.open(source) as img:
-            rgb_img = img.convert("RGB") # Convert to RGB
-            jpg_path = os.path.join(destination, os.path.splitext(os.path.basename(source))[0] + ".jpg") # Setup jpg path
-            rgb_img.save(jpg_path) # Save as JPG
+            rgb_img = img.convert("RGB")
+            jpg_path = os.path.join(destination, os.path.splitext(os.path.basename(source))[0] + '.jpg')
+
+            rgb_img.save(jpg_path)
 
             print(f"Converted: {source} ----> {jpg_path}")
 
--- tools/pngTojpg.py
+++ tools/pngTojpg.py
@@ -1,68 +1,55 @@
-from PIL import Image # pip install pillow
+from PIL import Image
 import os
-
+import csv
 
 def png_to_jpg():
 
-    input_option = input("How would you like to input the file names?\n(a) Manually enter file names\n(b) Paste file names\n(c) Import from CSV file\n\n>>>")
+    print("Choose the conversion method ->\n(a) Input source folder path\n(b) Import from a CSV file\n\n>>>")
 
-    # Enter the file source
-    source = input("Enter source:\n>> ")
+    input_option = input(">> ")
 
-    # Enter the destination path
-    destination = input("Enter destination:\n>> ")
+    source = input("Enter source of image directory:\n>>")
 
-    # If manual input option is chosen
+    destination = input("Enter destination:\n>>")
+
     if input_option == 'a':
-        print("Enter the file names: ")
 
-        while True:
-            current_name = input("Enter the current file name:\n>>")
-            if not current_name:
-                break
-            else:
-                convert_png_to_jpg(os.path.join(source, current_name), destination)
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                if file.lower().endswith('.png'):
+                    convert_png_to_jpg(os.path.join(root, file), destination)
 
-    # If paste multiple file names option is chosen
     elif input_option == 'b':
-        print("Paste file names:")
 
-        while True:
-            user_input = input(">> ").strip()
+        csv_file_path = input("Enter the path to CSV file:\n(sample format: 1st col -> file_name)\n>> ")
 
-            if not user_input:
-                break
-
-            files_to_be_converted.append(os.path.join(source, user_input))
-
-            for file_name in files_to_be_converted:
-                convert_heic_to_jpg(os.path.join(source, file_name), destination)
-
-    # If import from CSV option is chosen
-    elif input_option == 'c':
-        csv_file_path = input("Enter the path to the CSV file containing the file names:\n>>")
         try:
             with open(csv_file_path, newline='', encoding='utf-8') as csv_file:
+                reader = csv.reader(csv_file)
 
-                for row in csv_file:
-                    convert_png_to_jpg(os.path.join(source, row.strip()), destination)
+                for row in reader:
+                    if row:
+                        file_name = row[0].strip()
+                        convert_png_to_jpg(os.path.join(source, file_name), destination)
         
         except FileNotFoundError:
             print(f"Error: CSV file not found at {csv_file_path}")
             return
 
     else:
-        print("Invalid option. Enter 'a', 'b', or 'c'.")
+        print("Invalid option. Enter 'a' or 'b'.")
         return
+        
 
 def convert_png_to_jpg(source, destination):
     try:
-        # Access the image
         with Image.open(source) as img:
-            rgb_img = img.convert("RGB") # Convert to RBG first 
-            jpg_path = os.path.join(destination, os.path.splitext(os.path.basename(source))[0] + ".jpg") # Setup the jpg path
-            rgb_img.save(jpg_path) # Save image as jpg
-            print(f"Converted: {source} ----> {jpg_path}")
+            rgb_img = img.convert("RGB")
+            jpg_path = os.path.join(destination, os.path.splitext(os.path.basename(source))[0] + ".jpg")
 
+            rgb_img.save(jpg_path)
+
+            print(f"Converted: {source} ----> {jpg_path}")
+    
     except Exception as e:
         print(f"Error processing {source}: {e}")
